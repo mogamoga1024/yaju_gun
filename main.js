4e887ae4620bd219b7d219c1a73d4040aa15155e
mogamoga <mogamoga.g1thu6@gmail.com>
2025-03-16 17:18:22 +0900

    眩しい虹を渡れー

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const enemy = new Senpai(canvas.width / 2);
 enemy.draw();
 
 function update() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
     enemy.update();
     enemy.draw();
     requestAnimationFrame(update);
--- senpai.js
+++ senpai.js
@@ -3,34 +3,38 @@ class Senpai {
     #width = 0;
     #height = 0;
     #centerX = 0;
-    #zRate = 1;
-    #frameCount = 0;
+    #temaeRate = 1;
+    // #frameCount = 0;
 
-    constructor(centerX, zRate = 1) {
+    constructor(centerX, temaeRate = 1) {
         this.#centerX = centerX;
         this.#width = 200;
         this.#height = 400;
-        this.#zRate = zRate;
+        this.#temaeRate = temaeRate;
     }
 
     draw() {
-        const width = this.#width;
-        const height = this.#height;
+        const width = this.#width * this.#temaeRate;
+        const height = this.#height * this.#temaeRate;
 
-        const x = this.#centerX - this.#width / 2;
+        const x = this.#centerX - width / 2;
         const bottomY0 = canvas.height + 200;
         const bottomY = bottomY0;
-        const y = bottomY - height;
+        // this.#temaeRate: 1でoffsetY: 0
+        const offsetY = canvas.height * (1 - this.#temaeRate);
+        const y = bottomY - height - offsetY;
 
         context.fillStyle = "blue";
         context.fillRect(x, y, width, height);
+
+        console.log(offsetY);
     }
 
     update() {
-        if (this.#zRate <= 0) {
+        const temaeRateMin = 0.1;
+        if (this.#temaeRate <= temaeRateMin) {
             return;
         }
-        this.#frameCount++;
-        // todo
+        this.#temaeRate = Math.max(this.#temaeRate - 0.002, temaeRateMin);
     }
 }

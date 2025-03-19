030df6fab98ef519500285921f396cbea82298c8
mogamoga <mogamoga.g1thu6@gmail.com>
2025-03-19 19:44:59 +0900

    PC 操作性向上

--- main.js
+++ main.js
@@ -8,6 +8,22 @@ const enemyList = [];
 // 0で真正面 90で左 180で後ろ 270で右
 let viewAngle = 0;
 
+const isPC = (function() {
+    const mobileRegex = /iphone;|(android|nokia|blackberry|bb10;).+mobile|android.+fennec|opera.+mobi|windows phone|symbianos/i;
+    const isMobileByUa = mobileRegex.test(navigator.userAgent);
+    const isMobileByClientHint = navigator.userAgentData && navigator.userAgentData.mobile;
+    return !(isMobileByUa || isMobileByClientHint);
+})();
+
+const pc = {
+    isPressed: {
+        "left": false,
+        "right": false,
+        "turn": false,
+    },
+    canTurn: true,
+};
+
 loadImage("asset/草原.png").then(backgroundImage => {
     drawBackgroundImage(backgroundImage, viewAngle);
     enemyList.push(new RunningSenpai(canvas.width / 2));
@@ -18,6 +34,21 @@ loadImage("asset/草原.png").then(backgroundImage => {
     setupControls();
 
     function update() {
+        // PCでのキーイベント
+        if (isPC) {
+            const speed = 4;
+            if (pc.isPressed.left) {
+                viewAngle = (viewAngle + speed) % 360;
+            }
+            if (pc.isPressed.right) {
+                viewAngle = (viewAngle + (360 - speed)) % 360;
+            }
+            if (pc.isPressed.turn && pc.canTurn) {
+                pc.canTurn = false;
+                viewAngle = (viewAngle + 180) % 360;
+            }
+        }
+
         // 状態の更新
         enemyList.forEach(enemy => enemy.update());
         
@@ -44,30 +75,41 @@ loadImage("asset/草原.png").then(backgroundImage => {
 });
 
 function setupControls() {
-    const mobileRegex = /iphone;|(android|nokia|blackberry|bb10;).+mobile|android.+fennec|opera.+mobi|windows phone|symbianos/i;
-    const isMobileByUa = mobileRegex.test(navigator.userAgent);
-    const isMobileByClientHint = navigator.userAgentData && navigator.userAgentData.mobile;
-    const isPC = !(isMobileByUa || isMobileByClientHint);
-
     if (isPC) {
         window.addEventListener("keydown", e => {
             switch (e.key) {
                 case "ArrowLeft":
                 case "a": case "A":
-                    // todo
-                    viewAngle = (viewAngle + 10) % 360;
+                    pc.isPressed.left = true;
                     break;
 
                 case "ArrowRight":
                 case "d": case "D":
-                    // todo
-                    viewAngle = (viewAngle + (360 - 10)) % 360;
+                    pc.isPressed.right = true;
                     break;
                 
                 case "ArrowDown":
                 case "s": case "S":
-                    // todo
-                    viewAngle = (viewAngle + 180) % 360;
+                    pc.isPressed.turn = true;
+                    break;
+            }
+        });
+        window.addEventListener("keyup", e => {
+            switch (e.key) {
+                case "ArrowLeft":
+                case "a": case "A":
+                    pc.isPressed.left = false;
+                    break;
+
+                case "ArrowRight":
+                case "d": case "D":
+                    pc.isPressed.right = false;
+                    break;
+                
+                case "ArrowDown":
+                case "s": case "S":
+                    pc.isPressed.turn = false;
+                    pc.canTurn = true;
                     break;
             }
         });

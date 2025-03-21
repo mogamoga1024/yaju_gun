18163994ffe9f5c98b6e3333b0c0d50a71a377d7
mogamoga <mogamoga.g1thu6@gmail.com>
2025-03-21 22:51:58 +0900

    folder分け隊

--- storage.js
+++ storage.js
@@ -11,3 +11,15 @@ class ImageStorage {
     }
 }
 
+class SoundStorage {
+    static #dic = new Map();
+
+    static set(name, image) {
+        this.#dic.set(name, image);
+    }
+
+    static get(name) {
+        return this.#dic.get(name);
+    }
+}
+

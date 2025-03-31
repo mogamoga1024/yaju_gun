d09faef839e8af6be94061da201cac49a629fe9e
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-01 02:42:08 +0900

    game

--- src/main.js
+++ src/main.js
@@ -17,32 +17,32 @@ loadImage("asset/こちらを見つめる先輩.png").then(image => {
 // todo scene移植対応中…
 
 (async function() {
-    const promiseList = [];
-    const plpiss = (name) => {
-        promiseList.push((async () => ImageStorage.set(name, await loadImage(`asset/${name}.png`)))());
-    };
-    const plls = (name) => {
-        promiseList.push((async () => await loadSound(name))());
-    };
+    // const promiseList = [];
+    // const plpiss = (name) => {
+    //     promiseList.push((async () => ImageStorage.set(name, await loadImage(`asset/${name}.png`)))());
+    // };
+    // const plls = (name) => {
+    //     promiseList.push((async () => await loadSound(name))());
+    // };
 
-    // 画像
-    for (let i = 0; i <= 12; i++) {
-        plpiss(`走る野獣先輩/${i}`);
-    }
-    for (let i = 0; i <= 5; i++) {
-        plpiss(`くねくね先輩/${i}`);
-    }
-    plpiss("照準1");
-    plpiss("照準2");
-    plpiss("爆発スプライト_170");
+    // // 画像
+    // for (let i = 0; i <= 12; i++) {
+    //     plpiss(`走る野獣先輩/${i}`);
+    // }
+    // for (let i = 0; i <= 5; i++) {
+    //     plpiss(`くねくね先輩/${i}`);
+    // }
+    // plpiss("照準1");
+    // plpiss("照準2");
+    // plpiss("爆発スプライト_170");
     
-    // 音声 プリロード
-    // ※ Howler.jsにキャッシュさせておきたい
-    for (const name of ["ドンッ", "大破", "爆発", "息継ぎ", "アイスティー"]) {
-        plls(name);
-    }
+    // // 音声 プリロード
+    // // ※ Howler.jsにキャッシュさせておきたい
+    // for (const name of ["ドンッ", "大破", "爆発", "息継ぎ", "アイスティー"]) {
+    //     plls(name);
+    // }
 
-    await Promise.all(promiseList);
+    // await Promise.all(promiseList);
 
     // main(); // todo game_sceneで実行させたい
 })();
@@ -70,19 +70,19 @@ async function main() {
         mouseY: canvas.height / 2,
     };
 
-    const bgm = {
-        isFirst: true,
-        sound: null
-    };
-    window.addEventListener("click", async () => {
-        // return; // todo
-        if (!bgm.isFirst) {
-            return;
-        }
-        bgm.isFirst = false;
-        bgm.sound = await loadSound("PLUMBER");
-        playSound(bgm.sound);
-    });
+    // const bgm = {
+    //     isFirst: true,
+    //     sound: null
+    // };
+    // window.addEventListener("click", async () => {
+    //     // return; // todo
+    //     if (!bgm.isFirst) {
+    //         return;
+    //     }
+    //     bgm.isFirst = false;
+    //     bgm.sound = await loadSound("PLUMBER");
+    //     playSound(bgm.sound);
+    // });
 
     canvas.addEventListener("click", e => {
         hasShot = true;
--- src/scene/game_scene.js
+++ src/scene/game_scene.js
@@ -1,10 +1,16 @@
 
 class GameScene extends Scene {
     #backgroundImage = null;
+    #bgm = null;
 
     async onStart() {
         console.log("GameScene:onStart");
         this.#backgroundImage = await loadImage("asset/草原.png");
+        await this.#preload();
+
+        this.#bgm = await loadSound("PLUMBER");
+        playSound(this.#bgm);
+
         this.#update();
     }
 
@@ -12,6 +18,35 @@ class GameScene extends Scene {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         // todo
+    }
+
+    async #preload() {
+        const promiseList = [];
+        const plpiss = (name) => {
+            promiseList.push((async () => ImageStorage.set(name, await loadImage(`asset/${name}.png`)))());
+        };
+        const plls = (name) => {
+            promiseList.push((async () => await loadSound(name))());
+        };
+
+        // 画像
+        for (let i = 0; i <= 12; i++) {
+            plpiss(`走る野獣先輩/${i}`);
+        }
+        for (let i = 0; i <= 5; i++) {
+            plpiss(`くねくね先輩/${i}`);
+        }
+        plpiss("照準1");
+        plpiss("照準2");
+        plpiss("爆発スプライト_170");
+        
+        // 音声 プリロード
+        // ※ Howler.jsにキャッシュさせておきたい
+        for (const name of ["ドンッ", "大破", "爆発", "息継ぎ", "アイスティー"]) {
+            plls(name);
+        }
+
+        await Promise.all(promiseList);
     }
 
     onKeyDown(e) {

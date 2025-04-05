ec4a8aa2779767d8f85606e1d33940f324639dcf
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-06 01:02:26 +0900

    くるくる

--- src/entity/meteor_senpai.js
+++ src/entity/meteor_senpai.js
@@ -7,6 +7,7 @@ class MeteorSenpai extends Entity {
     #oriWidth = 0;
     #oriHeight = 0;
     #centerX = 0;
+    #angle = 0;
     #frameCount = 0;
     #imageList = [];
     #imageListIndex = 0;
@@ -32,16 +33,23 @@ class MeteorSenpai extends Entity {
     draw() {
         // todo sound
 
+        context.save();
         context.globalAlpha = this.#opacity;
+        context.translate(this.#x + this.#width / 2, this.#y + this.#height / 2);
+        context.rotate(this.#angle);
         const image = this.#imageList[this.#imageListIndex];
-        context.drawImage(image, this.#x, this.#y, this.#width, this.#height);
-        context.globalAlpha = 1;
+        // context.drawImage(image, this.#x, this.#y, this.#width, this.#height);
+        context.drawImage(image, -this.#width / 2, -this.#height / 2, this.#width, this.#height);
+        // context.globalAlpha = 1;
+        context.restore();
 
         this.#explosion?.draw(this.#x + this.#width / 2, this.#y + this.#height / 2, Math.max(this.#width, this.#height));
     }
 
     update(viewAngle) {
         // todo
+
+        this.#angle += 1 / 60;
     }
 
     isTargeted(crosshairX, crosshairY) { 
@@ -65,6 +73,7 @@ class MeteorSenpai extends Entity {
         }
 
         // todo 仮
+        // this.#x = 0;
         this.#y = 0;
     }
 }

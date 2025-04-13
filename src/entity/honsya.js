655a187b1197f1294826812b098ff1ca613a10c5
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-13 14:18:45 +0900

    honsya

---
+++ src/entity/honsya.js
@@ -0,0 +1,32 @@
+
+class Honsya extends Entity {
+    #x = 0;
+    #y = 0;
+    #width = 0;
+    #height = 0;
+    #image = null;
+    #frameCount = 0;
+    #opacity = 1;
+    #explosion = null;
+
+    constructor(centerX, viewAngle) {
+        // todo
+    }
+
+    draw() {
+        // todo
+    }
+
+    update(viewAngle) {
+        // todo
+    }
+
+    isTargeted(crosshairX, crosshairY) {
+        // todo
+        return false;
+    }
+
+    takeDamage() {
+        // todo
+    }
+}

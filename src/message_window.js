6ee46456b86ebad4406da9132482129d7c29dceb
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-19 21:43:41 +0900

    1

--- src/message_window.js
+++ src/message_window.js
@@ -1,2 +1,22 @@
 
+class MessageWindow {
+    #x = 0;
+    #y = 0;
+    #width = 0;
+    #height = 0;
 
+    constructor(x, y, width, height) {
+        this.#x = x;
+        this.#y = y;
+        this.#width = width;
+        this.#height = height;
+    }
+
+    drawWindow() {
+        // todo
+    }
+
+    drawText() {
+        // todo
+    }
+}

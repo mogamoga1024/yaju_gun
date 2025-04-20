b93c55cdcb00de8354fa12a8253d84fdfe28405d
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-20 15:13:45 +0900

    いきなり

--- src/asset/sound_config.js
+++ src/asset/sound_config.js
@@ -23,8 +23,8 @@ const kotodamaNameList = [];
 
     // BGM
     add1(b, "パラオナボーイ／feat.重音テト", "mp3", {volume: 0.9});
-    add1(b, "PLUMBER", "m4a", {volume: 0.3});
-    add1(b, "中華淫行", "m4a", {volume: 0.45});
+    // add1(b, "PLUMBER", "m4a", {volume: 0.3});
+    // add1(b, "中華淫行", "m4a", {volume: 0.45});
     add1(b, "snow prizm (Ketsupine mix)", "mp3", {volume: 0.2});
     add1(b, "「愛のカタチ」四部合唱【山崎まさゆき】", "mp3", {volume: 0.9});
     add1(b, "星空.flv", "m4a", {volume: 0.45});

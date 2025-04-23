7d168e0ea388dd1f2f737601379ce4494fac4885
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-23 13:47:39 +0900

    a

--- src/asset/sound_config.js
+++ src/asset/sound_config.js
@@ -26,7 +26,7 @@ const kotodamaNameList = [];
     add1(b, "snow prizm (Ketsupine mix)", "mp3", {volume: 0.2});
     add1(b, "「愛のカタチ」四部合唱【山崎まさゆき】", "mp3", {volume: 0.9});
     add1(b, "星空.flv", "m4a", {volume: 0.45});
-    add1(b, "Smart Boy(Daily Unchi Special Mix)", "mp3", {volume: 0.4});
+    add1(b, "Smart Boy(Daily Unchi Special Mix)", "mp3", {volume: 0.5});
     add1(b, "シン・怪文書アレンジ／さとうささら", "m4a", {volume: 0.6});
     add1(b, "Namagusa Monk", "m4a", {volume: 0.35});
     add1(b, "PLUMBER(qwerty Remix)", "m4a", {volume: 0.5});
@@ -39,7 +39,7 @@ const kotodamaNameList = [];
     add1(s, "銃声", "mp3", {volume: 0.3});
     add1(s, "大破", "mp3", {volume: 0.6});
     add1(s, "爆発", "m4a", {volume: 0.5});
-    add1(s, "横向くんだよ90度！", "mp3", {volume: 0.4});
+    add1(s, "横向くんだよ90度！", "mp3", {volume: 0.3});
     add1(s, "レベルアップ", "mp3", {volume: 0.15});
 
     // 野獣先輩

4ff41bfebf93e4632a408ae5c4b7343b61b790b8
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-23 13:13:16 +0900

    a

--- src/scene/gameplay_scene.js
+++ src/scene/gameplay_scene.js
@@ -398,9 +398,9 @@ class GameplayScene extends Scene {
         this.#shouldWarnLeft = false;
         this.#shouldWarnRight = false;
         for (const entity of this.#sortedEntityList()) {
-            if (entity.temaeRate < 0.3) {
-                continue;
-            }
+            // if (entity.temaeRate < 0.3) {
+            //     continue;
+            // }
             const {leftX, rightX} = entity.getXRange();
             if (leftX >= canvas.width) {
                 const overflowRight = leftX - canvas.width;

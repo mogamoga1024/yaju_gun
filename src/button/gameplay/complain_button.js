5bbb1f017063a42ecff2b69b4a239806c55888e0
mogamoga <mogamoga.g1thu6@gmail.com>
2025-04-28 05:04:31 +0900

    a

---
+++ src/button/gameplay/complain_button.js
@@ -0,0 +1,33 @@
+
+class ComplainButton extends Button {
+    #x = 0;
+    #y = 0;
+    #width = 0;
+    #height = 0;
+    #text = "";
+
+    constructor() {
+        super();
+        // todo
+    }
+    
+    draw() {
+        // todo
+    }
+
+    isTargeted(crosshairX, crosshairY) {
+        if (crosshairX < this.#x) {
+            return false;
+        }
+        if (crosshairX > this.#x + this.#width) {
+            return false;
+        }
+        if (crosshairY < this.#y - this.#height) {
+            return false;
+        }
+        if (crosshairY > this.#y + this.#height) {
+            return false;
+        }
+        return true;
+    }
+}

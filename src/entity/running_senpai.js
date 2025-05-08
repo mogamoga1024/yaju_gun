27cf7aff2092b8add301d2f0f29d2ba5ff209480
mogamoga <mogamoga.g1thu6@gmail.com>
2025-05-08 20:09:41 +0900

    調整

--- src/entity/running_senpai.js
+++ src/entity/running_senpai.js
@@ -64,7 +64,7 @@ class RunningSenpai extends Entity {
             this.temaeRate = temaeRateMax;
         }
         else {
-            let a = 0.002 + 0.002 * this.temaeRate;
+            let a = 0.002 + 0.0018 * this.temaeRate;
             a *= 1 + balanceFactor() / 100;
             this.temaeRate = Math.min(this.temaeRate + a, temaeRateMax);
         }

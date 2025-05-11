9c19ec86cfcf416b090bc70c19b4fbea5217a0aa
mogamoga <mogamoga.g1thu6@gmail.com>
2025-05-11 16:33:49 +0900

    もっともっと声出せ よそ見は駄目

--- src/main.js
+++ src/main.js
@@ -50,45 +50,30 @@ if (!isPC) {
     domNotice.style.display = "none";
     canvas.style.marginTop = "0";
 
-    // todo max width
     const iw = Math.max(window.innerWidth, window.innerHeight);
     const ih = Math.min(window.innerWidth, window.innerHeight);
-    const cw = iw * (ih / canvas.height);
-    if (cw > iw) {
-        console.log("a")
-        const ch = cw * (canvas.height / canvas.width);
-        const cw2 = cw * (ih / ch);
-        canvas.style.maxWidth = `${cw2}px`;
-    }
-    else if (cw < iw) {
-        console.log("b")
+    const ch = iw * (canvas.height / canvas.width);
+    if (ch > ih) {
+        const cw = canvas.width * (ih / canvas.height);
         canvas.style.maxWidth = `${cw}px`;
+    }
+    else {
+        canvas.style.maxWidth = `${iw}px`;
     }
 
     const showNoticeIfPortrait = () => {
         const angle = screen.orientation.angle;
         if (angle === 0 || angle === 180) {
-            // canvas.style.width = "100%";
-            // canvas.style.height = "auto";
             domNotice.style.display = "";
             domNotice.innerText = "スマホを横にして❤";
         }
         else {
-            // canvas.style.width = "auto";
-            // canvas.style.height = "100svh";
             domNotice.style.display = "none";
             window.scrollTo(0, 0);
         }
     };
     showNoticeIfPortrait();
     window.addEventListener("orientationchange", showNoticeIfPortrait);
-
-    // window.addEventListener("scroll", () => {
-    //     const angle = screen.orientation.angle;
-    //     if (angle === 90 || angle === 270) {
-    //         canvas.style.height = `${window.innerHeight}px`;
-    //     }
-    // });
 }
 
 if (!/android/i.test(navigator.userAgent)) {
